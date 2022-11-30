23a1d45b827c168a4252f37628525c5d1a16c43e
GustavsD <56320673+Gustizz@users.noreply.github.com>
2022-11-30 14:01:39 +0000

    brackets partially work

--- main.py
+++ main.py
@@ -24,11 +24,94 @@ class Math:
 
 
 def revElements(list, index):
-  list.pop(i + 1)
-  list.pop(i)
-  list.pop(i - 1)
+  list.pop(index + 1)
+  list.pop(index)
+  list.pop(index - 1)
 
-equation = "0+"
+  
+def SimplifyMultDiv(_equation):
+
+  newEquation = _equation
+  
+  #Execute mult and div
+  i = 0
+  while len(newEquation) > i:
+  
+    if newEquation[i] == "*":
+      a = float(newEquation[i - 1])
+      b = float(newEquation[i + 1])
+  
+      revElements(newEquation, i)
+      newEquation.insert(i - 1, Math.multiply(a, b))
+  
+      i = 0
+    elif newEquation[i] == "/":
+      a = float(newEquation[i - 1])
+      b = float(newEquation[i + 1])
+  
+      revElements(newEquation, i)
+      newEquation.insert(i - 1, Math.divide(a, b))
+    else:
+      i += 1
+
+  return newEquation
+
+  
+def SimplifyAddSubOps(_equation):
+  
+  newEquation = _equation
+  #Simplyfy add and sub operators - (+- = -, -- = +)
+  i = 0
+  while len(newEquation) > i:
+    if ((newEquation[i] in basicOps) and (newEquation[i + 1] in basicOps)):
+      curOp = newEquation[i]
+      nextOp = newEquation[i + 1]
+  
+      if (curOp == nextOp):
+        newEquation[i] = "+"
+        newEquation.pop(i + 1)
+      else:
+        newEquation[i] = "-"
+        newEquation.pop(i + 1)
+  
+      i = 0
+    else:
+      i += 1
+
+  return newEquation
+
+def SimplifyAddSub(_equation):
+
+  newEquation = _equation
+    
+  #Execute add and sub
+  i = 0
+  while len(newEquation) > i:
+  
+    if newEquation[i] == "+":
+      a = float(newEquation[i - 1])
+      b = float(newEquation[i + 1])
+  
+      revElements(newEquation, i)
+      newEquation.insert(i - 1, Math.add(a, b))
+  
+      i = 0
+    elif newEquation[i] == "-":
+      a = float(newEquation[i - 1])
+      b = float(newEquation[i + 1])
+  
+      revElements(newEquation, i)
+      newEquation.insert(i - 1, Math.subtract(a, b))
+    else:
+      i += 1
+
+  return newEquation
+
+  
+# This isolates the values from the operators so multiple digits can be used
+  
+
+equation = "(0+"
 equation += input("Enter your equation: ")
 equation = equation.replace(" ", "")
 
@@ -36,74 +119,68 @@ equation = equation.replace("*", " * ")
 equation = equation.replace("/", " / ")
 equation = equation.replace("+", " + ")
 equation = equation.replace("-", " - ")
-
-
+equation = equation.replace("(", " ( ")
+equation = equation.replace(")", " ) ")
+equation += " ) "
 
 sortedEq = []
 basicOps = ["+", "-"]
 
 sortedEq = equation.split()
-print(sortedEq)
 
-#Execute mult and div
-i = 0
-while len(sortedEq) > i:
 
-  if sortedEq[i] == "*":
-    a = float(sortedEq[i - 1])
-    b = float(sortedEq[i + 1])
 
-    revElements(sortedEq, i)
-    sortedEq.insert(i - 1, Math.multiply(a, b))
 
-    i = 0
-  elif sortedEq[i] == "/":
-    a = float(sortedEq[i - 1])
-    b = float(sortedEq[i + 1])
+bracketCount = equation.count("(")
+x = 0
+while bracketCount > x:
+  openBrackIndex = 0
+  closeBrackIndex = 0
+  
+  #Find latest open bracket
+  for i in range(len(sortedEq)):
+    if(sortedEq[i] == "("):
+      openBrackIndex = i
+  for i in range(len(sortedEq) - 1, 0, -1):
+    if(sortedEq[i] == ")"):
+      closeBrackIndex = i
 
-    revElements(sortedEq, i)
-    sortedEq.insert(i - 1, Math.divide(a, b))
+  #If openBrack and closeBrack are the same then no brackets are found
+  if(openBrackIndex != closeBrackIndex):
+    bracketEq = []
+    
+    for i in range(openBrackIndex + 1, closeBrackIndex, 1):
+      bracketEq.append(sortedEq[i])
+
+  
+    #print(sortedEq)
+    #print(str(openBrackIndex) + " | " + str(closeBrackIndex))
+    #print(bracketEq)
+      
+    bracketEq.insert(0, 0)
+    bracketEq.insert(1, "+")
+  
+    bracketEq = SimplifyMultDiv(bracketEq)
+    bracketEq = SimplifyAddSubOps(bracketEq)
+    bracketEq = SimplifyAddSub(bracketEq)
+
+    #Removes the old brackets equation from sortedEQ and inserts the simplified value
+    for i in range(closeBrackIndex - openBrackIndex + 1):
+      sortedEq.pop(openBrackIndex)
+      
+    sortedEq.insert(openBrackIndex, bracketEq[0])
+    x = 0
   else:
-    i += 1
+    x += 1
+  
+  
+  
+    
+sortedEq = SimplifyMultDiv(sortedEq)
+sortedEq = SimplifyAddSubOps(sortedEq)
+sortedEq = SimplifyAddSub(sortedEq)
 
-#Simplyfy add and sub operators - (+- = -, -- = +)
-i = 0
-while len(sortedEq) > i:
-  if ((sortedEq[i] in basicOps) and (sortedEq[i + 1] in basicOps)):
-    curOp = sortedEq[i]
-    nextOp = sortedEq[i + 1]
 
-    if (curOp == nextOp):
-      sortedEq[i] = "+"
-      sortedEq.pop(i + 1)
-    else:
-      sortedEq[i] = "-"
-      sortedEq.pop(i + 1)
 
-    i = 0
-    print(sortedEq)
-  else:
-    i += 1
-
-#Execute add and sub
-i = 0
-while len(sortedEq) > i:
-
-  if sortedEq[i] == "+":
-    a = float(sortedEq[i - 1])
-    b = float(sortedEq[i + 1])
-
-    revElements(sortedEq, i)
-    sortedEq.insert(i - 1, Math.add(a, b))
-
-    i = 0
-  elif sortedEq[i] == "-":
-    a = float(sortedEq[i - 1])
-    b = float(sortedEq[i + 1])
-
-    revElements(sortedEq, i)
-    sortedEq.insert(i - 1, Math.subtract(a, b))
-  else:
-    i += 1
 
 print("Your result is " + str(sortedEq[0]))
